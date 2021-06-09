7fc7c0bf7fd316ebe4230dae8893e063191bda4a
ben-jacobson <ben_jacobson@live.com>
2021-06-10 07:45:57 +1000

    finalised the intro to objects part of the course

--- Lab 02 - Object oriented programming/Lab 02-01 Introduction to objects.cpp
+++ Lab 02 - Object oriented programming/Lab 02-01 Introduction to objects.cpp
@@ -1,3 +1,34 @@
 #include <Arduino.h>
 
-/* TODO! *?
+class car {
+	public:
+		car(int seats, String make, String model, bool manual_transmission) {
+			_seats = seats; 
+			_make = make;  
+			_model = model; 
+			_manual_transmission = manual_transmission;
+			// intialize with a 0 miles odometer. 
+		}
+
+		void start_ignition(void) {
+			// start the car
+		}
+
+		void brake(void) {
+
+		}
+
+	private:
+		int _seats;
+		String _make, _model;
+		bool _manual_transmission;
+};
+
+void setup() {
+	car family_car(4, "Toyota", "Family+", false);
+	family_car.start_ignition();
+}
+
+void loop() {
+	// do nothing for now
+}
